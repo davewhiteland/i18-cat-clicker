2374f490511554024365eaaf5ab6151de7d74563
Dave Whiteland <davewhiteland@users.noreply.github.com>
2021-02-25 15:45:25 +0000

    fixup translations to produce a better po file

--- app.py
+++ app.py
@@ -10,64 +10,58 @@ babel = Babel(app)
 
 MIDDAY = 12
 
-def get_greeting():
-  greeting = gettext("Good ")
+def get_greeting(name=None):
   hour_now = datetime.now().time().hour
   if hour_now < MIDDAY: 
-    greeting += gettext("morning")
+    if name is None:
+      greeting = gettext("Good morning.")
+    else:
+      greeting = gettext("Good morning, %(name)s. ", name=name)
   else:
-    greeting += gettext("afternoon")
+    if name is None:
+      greeting = gettext("Good afternoon.")
+    else:
+      greeting = gettext("Good afternoon, %(name)s. ", name=name)
   return greeting
 
 
 @app.route('/')
 def index():
-  
   greeting = gettext("Hello world.")
-  
   return render_template('index.html', greeting=greeting)
 
 
 @app.route('/greeting')
 def greeting():
-  
-  return render_template('index.html', greeting=get_greeting() + ".")
+  return render_template('index.html', greeting=get_greeting())
 
 
 @app.route('/name', methods = ['GET', 'POST'])
 @app.route('/cats', methods = ['GET', 'POST'])
 def name():
-  
   if request.method == 'GET':
     return render_template('name.html',
       greeting    = gettext("Hello"),
+      # NOTE submit appears on a button sumitting a form that collects user's name
       button_text = gettext("submit"),
       name_label  = gettext("Your name:"),
     )
   elif request.method == 'POST':
     name = request.form['name']
     qty_cats = len([s for s in request.form if s.startswith('cat')])
-
-    greeting = get_greeting()
-    if name:
-      greeting += ", " + name
-    greeting += "."
-    
-    plural_s = "s" 
-    if qty_cats == 1:
-      plural_s = ""
-    else:
-      plural_s = "s"
-
-    cat_action = " ".join([gettext("You clicked on"), str(qty_cats), 
-      gettext("cat") + plural_s + "."])
-
+    greeting = get_greeting(name)
+    # NOTE: the user has previously clicked on a number of pictures of cats
+    cat_action = ngettext(
+      "You clicked on %(num)s cat.",
+      "You clicked on %(num)s cats.", qty_cats)
     return render_template('cats.html',
       greeting=greeting,
       name=name,
       cat_action=cat_action,
+      # NOTE this appears on the button the user must click to submit choice of cats
       button_text = gettext("meow"),
     )
+
 
 @app.route('/about')
 def about():
